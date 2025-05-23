0c954f651cd46e3a24fce0f26dcf39d144a5926c
jackytsoick <cheukkit@nycstudents.net>
2025-05-23 09:37:35 -0400

    Update partB.java

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,1 +1,14 @@
+public int dogWalkShift(int startHour, int endHour) {
+    int amtEarned = 0;
 
+    for (int hour = startHour; hour <= endHour; hour++) {
+        int dogsWalked = walkDogs(hour);
+
+        amtEarned += dogsWalked * 5;
+
+        if (dogs == maxDogs || (hour >= 9 && hour <= 17)) {
+            amtEarned += 3;
+        }
+    }
+        return amtEarned;
+}
