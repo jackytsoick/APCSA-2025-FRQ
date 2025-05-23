f5b3b0a7afcbd5bd9753394d2ed97983c0db1076
jackytsoick <cheukkit@nycstudents.net>
2025-05-23 09:47:53 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,10 @@
+public SumOrSameGame(int numRows, int numCols) {
+    Random rand = new Random();
+    puzzle = new int[numRows][numCols];
 
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            puzzle[i][j] = rand.nextInt(9) + 1;
+        }
+    }
+}
