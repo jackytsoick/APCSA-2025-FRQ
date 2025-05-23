58a94bde72bc5312bfe522460b41648380d899d8
jackytsoick <cheukkit@nycstudents.net>
2025-05-23 09:58:41 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,1 +1,10 @@
+public boolean clearPair(int row, int col) {
+    int targetValue = puzzle[row][col];
 
+    if (targetValue < 1 || targetValue > 9) {
+        return false;
+    }
+
+    for (int i = row; i < puzzle.length; i++) {
+        for (int j = 0; j < puzzle[i].length; j++) {
+            if (i == row && j == col) {
